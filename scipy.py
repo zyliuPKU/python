c25d3412d8081b76c3bdae8ea60146ddd98c4b34
zekeliu <zekepku0926@outlook.com>
2025-01-03 17:49:31 +0800

    Update scipy.py

--- scipy.py
+++ scipy.py
@@ -5,6 +5,7 @@ norm.ppf(0.95) #百分位对应的标准正态值
 norm.pdf(1.96) #概率密度
 norm.cdf(1.96) #累计概率
 
-#还有很多常用量
-from scipy.stats import skew,kurtosis
+#还有很多常用统计量
+from scipy.stats import skew,kurtosis,moment
 skew([1,2,3,4,5])
+moment(da,3)#对da的第一个维度计算三阶矩
