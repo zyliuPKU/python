b048ef8e7743fe132c6d5b04063bcd893a6a1ca2
zekeliu <zekepku0926@outlook.com>
2025-03-12 09:04:51 +0800

    Update multiprocessing.py

--- multiprocessing.py
+++ multiprocessing.py
@@ -4,9 +4,19 @@ def func(dates):
   return
   
 if __name__ == '__main__':
-    batch_size = 20
-    dates=range(1, 1000)
-    dates_batch=[dates[i:i+batch_size] for i in range(0, len(dates), batch_size)]
+    #1.Pool
+    args=[1,2,3]
     n=mlp.cpu_count()
     with mlp.Pool(n) as p:
-        p.map(func, dates_batch)
+        p.map(func, args)
+      
+    #2.Process
+    p=mlp.Process(target=func,args=(1,))
+    p.start()
+    p.join()
+  
+    #3.Queue
+    q=mlp.Queue()
+    q.put(1)
+    q.get()
+  
